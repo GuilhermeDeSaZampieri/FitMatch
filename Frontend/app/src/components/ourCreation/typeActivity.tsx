61c0162ff0856cdc89b2f53ed8ac705e53027570
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-04-17 23:59:04 -0300

    last commit in frontEnd

--- Frontend/app/src/components/ourCreation/typeActivity.tsx
+++ Frontend/app/src/components/ourCreation/typeActivity.tsx
@@ -1,7 +1,7 @@
 // import { Link } from "react-router";
 // <Link to="/Home/Listagem">
 // </Link>
-function TypeActivities({ data, isSelected }: any) {
+function TypeActivities({ data }: any) {
   console.log(data);
   return (
     <div className="flex items-center flex-col gap-2 w-[90px] h-[90px]">
@@ -11,8 +11,7 @@ function TypeActivities({ data, isSelected }: any) {
           alt="atividade"
           className="rounded-full w[80px] h-[80px]
           transition-transform duration-300 ease-in-out hover:scale-105 hover:rotate-1"
-  
-        />        
+        />
       </div>
 
       <div className="flex justify-center">
